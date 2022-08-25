cd10860d0d9b232bcf649c22f5ac9ef94a96bdac
Gabriel de Souza Scopel <93132666+Gabriel-Scopel@users.noreply.github.com>
2022-08-25 14:36:52 -0300

    produzindo login

---
+++ src/handlers/LoginHandler.php
@@ -0,0 +1,31 @@
+<?php
+namespace src\handlers;
+use \src\models\User;
+//classe específica para fazer verificações de login
+class LoginHandler {
+    public static function checkLogin(){
+        if(!empty($_SESSION['token'])){ //verficando se há algum token na sessão
+            $token = $_SESSION['token'];
+            $data = User::select()->where('token, $token')->one();//verificando de qual usuário é aquele token
+            if(count($data)>0){
+                $loggedUser = new User();
+                $loggedUser->Id = ($data['id']);
+                $loggedUser->email = ($data['email']);
+                $loggedUser->name = ($data['name']);
+                return $loggedUser;
+            }
+        }
+        return false;
+    }
+    public static function verifyLogin($email, $password){
+        $user = User::select()->where('email', $email)->one();
+        if($user){
+            if(password_verify($password, $user['password'])){
+                $token = md5(time().rand(0,9999).time());
+                User::update()->set('token, $token')->where('email', $email)->execute();
+                return $token;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/models/User.php
@@ -0,0 +1,7 @@
+<?php //model responsável por identificar usuário do sistema no banco de dados
+namespace src\models;
+use \core\Model;
+
+class User extends Model {
+
+}
