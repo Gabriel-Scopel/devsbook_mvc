99027bb5fa5426ed974ce3e1a64b2267921a9769
Gabriel de Souza Scopel <93132666+Gabriel-Scopel@users.noreply.github.com>
2022-08-26 13:54:05 -0300

    adicionando sistema de cadastro

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -11,7 +11,7 @@ class LoginController extends Controller {
             $flash = $_SESSION['flash'];
             $_SESSION['flash'] = '';
         }
-        $this->render('login', [
+        $this->render('signin', [
             'flash'=>$flash
         ]);
     }
@@ -35,9 +35,45 @@ class LoginController extends Controller {
             $this->redirect('/login');
         }
     }
-    public function signup(){
-        echo 'cadastro';
-    }
     
+    public function signup(){
+        $flash = '';
+        if(!empty($_SESSION['flash'])){
+            $flash = $_SESSION['flash'];
+            $_SESSION['flash'] = '';
+        }
+        $this->render('signup', [
+            'flash'=>$flash
+        ]);
+    }
+    public function signupAction(){
+        $name = filter_input(INPUT_POST, 'name');
+        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+        $password = filter_input(INPUT_POST, 'password');
+        $birthdate = filter_input(INPUT_POST, 'birthdate');
+        if($name && $email && $password){
+            $birthdate = explode('/', $birthdate);
+            if(count($birthdate) != 3){
+                $_SESSION['flash'] = 'Data de nascimento inválida';
+                $this->redirect('/cadastro');
+            }
+            $birthdate = $birthdate[2].'-'.$birthdate[1].'-'.$birthdate[0];
+            if(strtotime($birthdate) === false){
+                $_SESSION['flash'] = 'Data de nascimento inválida';
+                $this->redirect('/cadastro');
+            }
+            if(LoginHandler::emailExists($email) === false){
+                $token = LoginHandler::addUser($name, $email, $password, $birthdate);
+                $_SESSION['token'] = $token;
+                $this->redirect('/');
+            }else{
+                $_SESSION['flash'] = 'E-mail já cadastrado';
+                $this->redirect('/cadastro');
+            }
+        }else{
+            $this->redirect('/cadastro');
+        }
+
+    }
 
 }
